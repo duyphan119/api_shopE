3957860701d6ce20e61efe6125e43c57fde1e74b
Nguyễn Hữu An <nguyenhuuan2727@gmail.com>
2022-07-06 16:51:07 +0700

    first commit

---
+++ src/server.js
@@ -0,0 +1,9 @@
+const express = require("express");
+const app = express();
+require("dotenv").config();
+
+const port = process.env.PORT || 1010;
+
+app.listen(port, () => {
+  console.log("app is listening 1", port);
+});
